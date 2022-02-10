c73736252000747217e747b63a16ce0356b36eba
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-02-10 16:47:37 +0600

    Create simple_bot.py

---
+++ examples/simple_bot.py
@@ -0,0 +1,10 @@
+import prjz
+
+ProjectZ = prjz.Client()
+ProjectZ.login("", "")
+
+ProjectZ.send_message(p.get_joined_chats()["list"][0]["threadId"], "Welcome")
+
+@p.on()
+def event(data):
+    ProjectZ.send_message(data["msg"]["threadId"], f"Hello {data['author']['nickname']}")
